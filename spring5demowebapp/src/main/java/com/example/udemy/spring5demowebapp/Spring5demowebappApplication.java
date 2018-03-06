9394c5595ebd39451fb652a66136d532f2c9d832
SergeA <ronnieo@gmail.com>
2018-03-05 21:25:24 -0500

    Spring5 WebApp Demo

---
+++ spring5demowebapp/src/main/java/com/example/udemy/spring5demowebapp/Spring5demowebappApplication.java
@@ -0,0 +1,12 @@
+package com.example.udemy.spring5demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5demowebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5demowebappApplication.class, args);
+	}
+}
