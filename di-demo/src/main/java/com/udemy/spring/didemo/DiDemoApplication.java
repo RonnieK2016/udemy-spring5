72a8fb6eb2695b0d2d70757d3b4575cfd2d626a4
SergeA <ronnieo@gmail.com>
2019-02-21 19:28:50 -0500

    Dependency-injection starter code

---
+++ di-demo/src/main/java/com/udemy/spring/didemo/DiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+}
