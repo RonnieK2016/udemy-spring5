b350b3e10317a997e46b96b61ebdfee1bc7a67e4
SergeA <ronnieo@gmail.com>
2019-03-12 12:09:45 -0400

    Added testMVC method

--- recipe-app/src/test/java/com/udemy/sfg/recipeapp/controllers/IndexPageControllerTest.java
+++ recipe-app/src/test/java/com/udemy/sfg/recipeapp/controllers/IndexPageControllerTest.java
@@ -7,6 +7,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +18,9 @@ import java.util.Set;
 import static org.junit.Assert.*;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexPageControllerTest {
 
@@ -28,6 +34,14 @@ public class IndexPageControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         indexPageController = new IndexPageController(recipeService);
+    }
+
+    @Test
+    public void testMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexPageController).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
