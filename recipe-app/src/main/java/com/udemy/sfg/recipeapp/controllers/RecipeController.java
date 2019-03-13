c04c5e4d852d5f89b433214d81e4ee14cfef2149
SergeA <ronnieo@gmail.com>
2019-03-13 14:05:59 -0400

    Added create recipe form

--- recipe-app/src/main/java/com/udemy/sfg/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/com/udemy/sfg/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package com.udemy.sfg.recipeapp.controllers;
 
+import com.udemy.sfg.recipeapp.commands.RecipeCommand;
 import com.udemy.sfg.recipeapp.domain.Recipe;
 import com.udemy.sfg.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +24,18 @@ public class RecipeController {
             Model model) {
         Recipe recipe = recipeService.getById(id);
         model.addAttribute("recipe", recipe);
-        return "show_recipe";
+        return "recipe/show_recipe";
+    }
+
+    @RequestMapping("recipe/new")
+    public String showRecipeForm(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String addOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
